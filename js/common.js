86a6e5b770732366173c832a582e0215261c25d5
BayramovNicat <bayramov.nicat@hotmail.com>
2016-01-16 12:38:31 +0400

    Slider

--- js/common.js
+++ js/common.js
@@ -2,10 +2,16 @@ $('#hot-deals-slider').slick({
   dots: true,
   infinite: true,
   speed: 500,
-  cssEase: 'linear'
+  fade: true,
+  cssEase: 'ease',
+  autoplay: true,
+  autoplaySpeed: 3000,
 });
 
 $('#sell-out').imagesLoaded( function() {
 	$('.product-img').imagefill({target:'img'}); 
 });
 
+$(document).on('click','#sidebar-wrapper-open',function(){
+	$('#wrapper').toggleClass('sidebar-wrapper-opened');
+});
