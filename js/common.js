20593926340bbd605e8d39d6aca0b177d78387a7
BayramovNicat <bayramov.nicat@hotmail.com>
2016-01-21 18:16:44 +0400

    Mobil version

--- js/common.js
+++ js/common.js
@@ -1,17 +1,14 @@
-$('#hot-deals-slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'ease',
-  autoplay: true,
-  autoplaySpeed: 3000,
-});
-
-$('#sell-out').imagesLoaded( function() {
-	$('.product-img').imagefill({target:'img'}); 
-});
-
+function slider_init(){
+	$('#hot-deals-slider').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'ease',
+	  autoplay: true,
+	  autoplaySpeed: 3000,
+	});
+}
 
 function sidebar_open(){
 	$('#sidebar-wrapper-open').on('click',function(){
@@ -23,6 +20,62 @@ function sidebar_open(){
 	});
 }
 
+function search_button(){
+	$(document).on('click','#search-button',function(){
+		$('.search').addClass('opened');
+		$('#header-main .row > div > a').hide();
+
+		$('html').click(function(event) {
+	        //check up the tree of the click target to check whether user has clicked outside of menu
+	        if ($(event.target).parents('.search').length == 0) {
+	           	$('.search').removeClass('opened');
+				$('#header-main .row > div > a').show();
+	            //this event listener has done its job so we can unbind it.
+	            $(this).unbind(event);
+	        }
+
+	    });
+	});
+}
+
+function sidebar_button(){
+	$(document).on('click','#open-mini-sidebar',function(){
+		$('#sidebar-wrapper').addClass('opened');
+
+		$('html').click(function(event) {
+	        //check up the tree of the click target to check whether user has clicked outside of menu
+	        if ($(event.target).parents('#sidebar-wrapper').length == 0) {
+	           	$('#sidebar-wrapper').removeClass('opened');
+	            //this event listener has done its job so we can unbind it.
+	            $(this).unbind(event);
+	        }
+
+	    });
+	});
+}
+
 $(document).ready(function(){
+	slider_init();
 	sidebar_open();
+	search_button();
+	sidebar_button();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
